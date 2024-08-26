023b715ffce65879d923ededb167be4514350ef4
vignesh-prakash <vignesh24p@gmail.com>
2024-08-26 16:10:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -5,25 +5,52 @@ int isBatteryInRange(float value, float min, float max) {
     return (value >= min && value <= max);
 }
 
+int IstemperatureOk(int value) {
+    if (!value) {
+        printf("Temperature out of range!\n");
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+int IssocOk(int value) {
+    if (!value) {
+        printf("State of Charge out of range!\n");
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
+int IschargeRateOk(int value) {
+    if (!value) {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     int temperatureOk = isBatteryInRange(temperature, 0, 45);
     int socOk = isBatteryInRange(soc, 20, 80);
     int chargeRateOk = isBatteryInRange(chargeRate, 0, 0.8);
 
-    if (!temperatureOk || !socOk || !chargeRateOk) {
-        if (!temperatureOk) {
-            printf("Temperature out of range!\n");
-        }
-        if (!socOk) {
-            printf("State of Charge out of range!\n");
-        }
-        if (!chargeRateOk) {
-            printf("Charge Rate out of range!\n");
-        }
+    int value1 = IstemperatureOk(temperatureOk);
+    int value2 = IssocOk(socOk);
+    int value3 = IschargeRateOk(chargeRateOk);
+    
+    if (!value1 || !value2 || !value3) {
         return 0;
     }
     return 1;
 }
+
+
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
