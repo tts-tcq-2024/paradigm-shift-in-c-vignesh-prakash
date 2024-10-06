0dd11e11b70f86324bb785215b892feb4400cebf
vignesh-prakash <vignesh24p@gmail.com>
2024-10-06 14:50:51 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,52 +1,6 @@
 #include <stdio.h>
-#include <assert.h>
-
-#define FREEZING_POINT_MIN 0
-#define FREEZING_POINT_MAX 2.25
-#define PEAK_TEMPERATURE_MIN 42.75
-#define PEAK_TEMPERATURE_MAX 45
-
-#define SOC_LOW_MIN 20
-#define SOC_LOW_MAX 24
-#define SOC_HIGH_MIN 76
-#define SOC_HIGH_MAX 80
-
-#define CHARGE_RATE_PEAK_MIN 0.76
-#define CHARGE_RATE_PEAK_MAX 0.8
-
-void WarnApproachingFreezing(float temperature) {
-    if (temperature >= FREEZING_POINT_MIN && temperature <= FREEZING_POINT_MAX) {
-        printf("Warning: Approaching freezing point\n");
-    }
-}
-
-void WarnApproachingPeakTemperature(float temperature) {
-    if (temperature >= PEAK_TEMPERATURE_MIN && temperature <= PEAK_TEMPERATURE_MAX) {
-        printf("Warning: Approaching peak temperature\n");
-    }
-}
-
-void GenerateTemperatureWarning(float temperature) {
-    WarnApproachingFreezing(temperature);
-    WarnApproachingPeakTemperature(temperature);
-}
-
-void WarnApproachingDischarge(float soc) {
-    if (soc >= SOC_LOW_MIN && soc <= SOC_LOW_MAX) {
-        printf("Warning: Approaching discharge\n");
-    }
-}
-
-void WarnApproachingChargePeak(float soc) {
-    if (soc >= SOC_HIGH_MIN && soc <= SOC_HIGH_MAX) {
-        printf("Warning: Approaching charge-peak\n");
-    }
-}
-
-void GenerateSOCWarning(float soc) {
-    WarnApproachingDischarge(soc);
-    WarnApproachingChargePeak(soc);
-}
+#include "Checker.h"
+#include "EarlyWarnings.h"
 
 int IsTemperatureInRange(float temperature) {
     return temperature >= FREEZING_POINT_MIN && temperature <= PEAK_TEMPERATURE_MAX;
@@ -54,12 +8,6 @@ int IsTemperatureInRange(float temperature) {
 
 int IsSOCInRange(float soc) {
     return soc >= SOC_LOW_MIN && soc <= SOC_HIGH_MAX;
-}
-
-void GenerateChargeRateWarning(float chargeRate) {
-    if (chargeRate >= CHARGE_RATE_PEAK_MIN && chargeRate <= CHARGE_RATE_PEAK_MAX) {
-        printf("Warning: Approaching peak ChargeRate\n");
-    }
 }
 
 int IsChargeRateInRange(float chargeRate) {
@@ -107,11 +55,4 @@ int IsChargeRateOk(float chargeRate) {
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
     return IsTemperatureOk(temperature) && IsSOCOk(soc) && IsChargeRateOk(chargeRate);
-}
-
-int main() {
-    assert(batteryIsOk(0.2, 20, 0.77));
-    assert(batteryIsOk(25, 70, 0.7));
-    assert(!batteryIsOk(50, 85, 0));
-    assert(!IsChargeRateOk(0.9));
 }
